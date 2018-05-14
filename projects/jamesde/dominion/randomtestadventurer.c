1041cc475e23ac647126a25afe1f072ef41d0a58
Devon James <jamesde@flip3.engr.oregonstate.edu>
2018-05-13 20:54:01 -0700

    Assignment 4 Changes

---
+++ projects/jamesde/dominion/randomtestadventurer.c
@@ -0,0 +1,133 @@
+#include "assert.h"
+#include "dominion.h"
+#include <stdio.h>
+#include "rngs.h"
+#include <stdlib.h>
+#include <time.h>
+
+int main (int argc, char** argv) {
+	struct gameState Game;
+	int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, 
+	             sea_hag, tribute, smithy};
+    int numberPlayers = 2;
+	int currentPlayer = 0;
+	int deckSize, handSize, a, b, c, d, x, randomCard, randomK, coinPre, coinPost, discardCopper, discardSilver, discardGold;
+	int passed = 0;
+	int drawFail = 0;
+	int discardFail = 0;
+	int testPassed = 0;
+	int temphand[MAX_HAND];
+	int z = 0;
+	
+	srand(time(NULL));
+	
+	//Choose 10000 since it takes about a minute to run
+	for(a = 0; a < 10000; a++){
+		initializeGame(numberPlayers, k, 1000, &Game);
+		coinPre = 0;
+		deckSize = rand() % (MAX_DECK + 1);
+		handSize = rand() % (deckSize + 1);
+		
+		Game.deckCount[0] = deckSize - handSize;
+		Game.handCount[0] = handSize;
+		
+		for(b = 0; b < numberPlayers; b++){
+			for(c = 0; c < Game.deckCount[b]; c++){
+				randomCard = rand() % 51;
+				randomK = rand() % 10;
+				if(randomCard == 1){
+					Game.deck[b][c] = copper;
+				}
+				else if(randomCard == 2){
+					Game.deck[b][c] = silver;
+				}
+				else if(randomCard == 3){
+					Game.deck[b][c] = gold;
+				}
+				else{
+					Game.deck[b][c] = k[randomK];
+				}
+			}
+		}
+		
+		for(d = 0; d < Game.handCount[currentPlayer]; d++){
+			if(Game.hand[currentPlayer][d] == copper || Game.hand[currentPlayer][d] == silver || Game.hand[currentPlayer][d] == gold){
+			coinPre++;
+		}
+		
+	}
+	
+	printf("Treasure Card Count before: %d\n", coinPre);
+	printf("Discard Count before: %d\n", Game.discardCount[currentPlayer]);
+
+	//Refactor function 
+	adventurerCard(currentPlayer, temphand, z, &Game);
+    
+	coinPost = 0;
+	
+	for(d = 0; d < Game.handCount[currentPlayer]; d++){
+			if(Game.hanRandom Testing
+d[currentPlayer][d] == copper || Game.hand[currentPlayer][d] == silver || Game.hand[currentPlayer][d] == gold){
+			coinPost++;
+		}
+	}
+	
+	printf("Treasure Card Count after: %d\n", coinPost);
+	printf("Discard Count after: %d\n", Game.discardCount[currentPlayer]);
+	
+    discardCopper = 0;
+    discardSilver = 0;
+    discardGold = 0;
+    for(x = 0; x < Game.discardCount[currentPlayer]; x++){
+            if(Game.discard[currentPlayer][x] == copper) {
+                discardCopper++;
+            } else if(Game.discard[currentPlayer][x] == silver) {
+                discardSilver++;
+            } else if(Game.discard[currentPlayer][x] == gold) {
+                discardGold++;
+            }
+    }
+	int passed = 1;
+	
+	if(coinPost > coinPre + 2){
+		printf("Too many cards were drawn: FAILED!!\n");
+		drawFail++;
+		passed = 0;
+	}
+	
+	if(coinPost < coinPre){
+		printf("Too few cards were drawn: FAILED!!\n");
+		drawFail++;
+		passed = 0;
+	}
+	
+	if(discardCopper != 0){
+            printf("Copper was discarded: FAILED\n");
+            discardFail++;
+            passed = 0;
+    }
+
+    if(discardSilver != 0){
+            printf("Silver was discarded: FAILED\n");
+            discardFail++;
+            passed = 0;
+    }
+
+    if(discardGold != 0){
+            printf("Gold was discarded: FAILED\n");
+            discardFail++;
+            passed = 0;
+    }
+
+	if(passed == 1){
+		printf("All Tests PASSED\n");
+		testPassed++;
+	}
+	
+	}
+	printf("Number of Tests Passed: %d\n", testPassed);
+	printf("Number of Treasure Cards Drawn To Player's hand Failed: %d\n", drawFail);
+	printf("Number of Adventure Cards Discareded Fails: %d\n", discardFail);
+	
+	return 0;
+}
---
+++ projects/jamesde/dominion/randomtestcard2.c
@@ -0,0 +1,83 @@
+#include "assert.h"
+#include "dominion.h"
+#include <stdio.h>
+#include "rngs.h"
+#include <stdlib.h>
+#include <time.h>
+
+
+int main (int argc, char** argv) {
+	struct gameState Game;
+	int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, 
+	             sea_hag, tribute, smithy};
+    int numberPlayers = 2;
+	int currentPlayer = 0;
+	int deckSize, handSize, deckPre, deckPost, handPre, handPost, handPostion, discardPre, discardPost, i, j;
+	int passed;
+	int handFail = 0;
+	int deckFail = 0;
+	int discardFail = 0;
+	int testResult = 0;
+    
+	srand(time(NULL));
+	printf("********Running Random Test for Smithy Card********\n");
+	for(i = 0; i < 10000; i++){
+		initializeGame(numberPlayers, k, 1000, &Game);
+		deckSize = rand() % (MAX_DECK + 1);
+		handSize = rand() % (deckSize + 1);
+		
+		Game.deckCount[0] = deckSize - handSize;
+		Game.handCount[0] = handSize;
+		
+		handPostion = Game.hand[currentPlayer][Game.handCount[currentPlayer] - 1];
+		
+		deckPre = Game.deckCount[0];
+		printf("Deck Count Before: %d\n", deckPre);
+		handPre = Game.handCount[0];
+		printf("Hand Count Before: %d\n", handPre);
+		discardPre = Game.playedCardCount;
+		printf("Discarded Cards Before: %d\n", discardPre);
+		
+		//Refactor function
+		smithyCard(currentPlayer, handPostion, &Game);
+		
+		deckPost = Game.deckCount[0];
+		printf("Deck Count After: %d\n", deckPost);
+		handPost = Game.handCount[0];
+		printf("Hand Count After: %d\n", handPost);
+		discardPost = Game.playedCardCount;
+		printf("Discarded Cards After: %d\n", discardPost);
+		
+		passed = 1;
+		
+		if(handPost != (handPre + 2)){
+			printf("Wrong number of cards were drawn: FAILED!!\n");
+			handFail++;
+			passed = 0;
+		}
+		
+		if(deckPost != (deckPre - 3)){
+			printf("Wrong number of cards were removed from player's deck: FAILED!!\n");
+			deckFail++;
+			passed = 0;
+		}
+		
+		if(discardPost != (discardPre + 1)){
+			printf("Smithy Card was not discarded: FAILED!!\n");
+			discardFail++;
+			passed = 0;
+		}
+			
+		if(passed == 1){
+			printf("Smithy Card Test PASSED!!\n");
+			testResult++;
+		}
+	}
+ 
+ printf("Number of Tests Passed: %d\n", testResult);
+ printf("Incorrect amount in Player's deck: %d\n", deckFail);
+ printf("Number of Smithy Cards Drawn to Hand Failed: %d\n", handFail);
+ printf("Number of Smithy Cards Discarded Failed: %d\n", discardFail);
+ 
+ return 0;
+}
